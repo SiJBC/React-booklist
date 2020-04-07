9f7e13454285e9410ec04eb226e470912e6a6012
Simon Colman <s_colman@hotmail.com>
2020-04-07 10:20:52 +1000

    deploy

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
+require("dotenv").config()
+
 const express = require("express");
 
 const mongoose = require("mongoose");
 const routes = require("./routes");
 const app = express();
 const PORT = process.env.PORT || 3001;
+const username = process.env.db_username
+const password = process.env.db_password
 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
